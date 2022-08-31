f9be39ff4ecb1ffdd48c1946bb359df706a20a27
mihayounas <mihaelayounas55@gmail.com>
2022-08-31 23:08:22 +0000

    Putting in place and creating views for post view and home page

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="home"),
-    path('<slug:slug>', views.PostDetail.as_view(), name="post_detail")
+    path('', views.homepage, name='homepage'),
+    path('post_view', views.PostList.as_view(), name="post_view"),
+    path('<slug:slug>', views.PostDetail.as_view(), name="post_detail"),
+    path('<slug:slug>', views.PostLike.as_view(), name="post_like"),
+
 ]
