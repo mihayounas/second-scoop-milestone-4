de269530d84131830bca6af8e85deb5e1f74e175
mihayounas <mihaelayounas55@gmail.com>
2022-09-09 10:45:50 +0000

    Work on reservations view

--- blog/views.py
+++ blog/views.py
@@ -92,13 +92,15 @@ def Reserve(View):
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
+        reservations = post.reservations.filter(
+            approved=True).order_by("-created_on")
 
         return render(
             request,
             "reservations.html",
             {
-                "reservation_form": ReservationForm()
+                "reservation_form": ReservationForm(),
+
             },
         )
 
@@ -113,12 +115,15 @@ def Reserve(View):
             reservations.post = post
             reservations.save()
         else:
-            reservations_form = ReservationsForm
+            reservations_form = ReservationsForm(request)
 
         return render(
             request,
             "reservations.html",
             {
                 "reservations_form": reservations_form,
+                "reservations": reservations,
             },
         )
+
+        return HttpResponseRedirect(reverse('reservations', args=[request]))
