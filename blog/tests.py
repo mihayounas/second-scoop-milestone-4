337f958e8d78ea68367a2bc7f9ec89f889138792
mihayounas <mihaelayounas55@gmail.com>
2022-10-22 23:13:17 +0000

    Run Tests

--- blog/tests.py
+++ blog/tests.py
@@ -5,9 +5,16 @@ from .forms import Booking
 # Create your tests here.
 
 
+# class TestPost(TestCase):
+
+
+# class TestComment(TestCase):
+
+
+# class TestContact(TestCase):
+
+
 class ReservationsTestCase(TestCase):
     def test_name_required(self):
         form = Booking({'phone': ''})
         self.assertFalse(form.is_valid())
-        self.asesertIn('phone', forms.errors.keys())
-        self.assertEqual(form.errors['phone'][0], 'Please fill in this field.')
