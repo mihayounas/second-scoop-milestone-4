d2f6a15fbce37a688b5b88245bc8e7fe1b87839f
mihayounas <mihaelayounas55@gmail.com>
2022-10-28 11:20:51 +0000

    Fix comments delete button to show only for comment author or admin

--- blog/models.py
+++ blog/models.py
@@ -43,6 +43,8 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="comments_field", null=True)
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
