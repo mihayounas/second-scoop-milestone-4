6b413ad620a8eb49c28c543bd60a16ac7f9fb696
mihayounas <76411576+mihayounas@users.noreply.github.com>
2023-03-27 09:35:59 +0000

    changed db

--- secondscoop/settings.py
+++ secondscoop/settings.py
@@ -115,17 +115,17 @@ WSGI_APPLICATION = 'secondscoop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
 }
+
+# DATABASE_URL = os.getenv('DATABASE_URL')
+# DATABASES = {
+#    'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
+#}
 
 
 # Password validation
